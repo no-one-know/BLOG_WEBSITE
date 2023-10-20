f95aa2b354700c483f0b4b190ee3731a8e8f937e
Chetan Bagra <bagra200chetan@gmail.com>
2023-10-20 23:24:57 +0530

    env file updated

--- blog_website/config.py
+++ blog_website/config.py
@@ -1,12 +1,14 @@
 import os
+from decouple import config
+
 path='sqlite:///'+os.getcwd()
 
 class Config:
-    SECRET_KEY='0c0fa473989cd4d32b4d392bad1b28'
-    WTF_CSRF_SECRET_KEY = '1c9fa173969ct4d32r4d392bad1b69'
+    SECRET_KEY=config('SECRET_KEY')
+    WTF_CSRF_SECRET_KEY = config('WTF_CSRF_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI=os.path.join(path,'database.db')
-    MAIL_SERVER='smtp.googlemail.com'
-    MAIL_PORT=587
-    MAIL_USE_TLS=True
-    MAIL_USERNAME=os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD=os.environ.get('EMAIL_PASS')
+    MAIL_SERVER=config('MAIL_SERVER')
+    MAIL_PORT=config('MAIL_PORT',cast=int)
+    MAIL_USE_TLS=config('MAIL_USE_TLS',cast=bool)
+    MAIL_USERNAME=config('MAIL_USERNAME')
+    MAIL_PASSWORD=config('MAIL_PASSWORD')
