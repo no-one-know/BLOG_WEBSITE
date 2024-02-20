1000285059d3b1f4fd08708ca6354eaae6a1323f
Chetan Bagra <bagra200chetan@gmail.com>
2024-02-21 02:26:23 +0530

    change in database

--- blog_website/config.py
+++ blog_website/config.py
@@ -1,12 +1,9 @@
-import os
 from decouple import config
-
-path='sqlite:///'+os.getcwd()
 
 class Config:
     SECRET_KEY=config('SECRET_KEY')
     WTF_CSRF_SECRET_KEY = config('WTF_CSRF_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI=os.path.join(path,'database.db')
+    SQLALCHEMY_DATABASE_URI=config('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER=config('MAIL_SERVER')
     MAIL_PORT=config('MAIL_PORT',cast=int)
     MAIL_USE_TLS=config('MAIL_USE_TLS',cast=bool)
